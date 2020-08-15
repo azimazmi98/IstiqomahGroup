ee92efad5bd86e27be86cfa50129b850d66fdfec
ahmadhuzaifi98 <44748431+ahmadhuzaifi98@users.noreply.github.com>
2020-08-15 14:12:56 +0800

    Update App.js

--- App.js
+++ App.js
@@ -22,7 +22,7 @@ export default class App extends Component {
     return (
     
       
-      <View style={styles.container}>
+      <View style={Styles.container}>
   
         <View style={Styles.container}>
 
@@ -34,39 +34,22 @@ export default class App extends Component {
           value={this.state.username}
           onChangeText={(username) => this.setState({ username })}
           placeholder={'Username'}
-          style={styles.input}
+          style={Styles.input}
         />
         <TextInput
           value={this.state.password}
           onChangeText={(password) => this.setState({ password })}
           placeholder={'Password'}
           secureTextEntry={true}
-          style={styles.input}
+          style={Styles.input}
         />
         
         <Button
           title={'Login'}
-          style={styles.input}
+          style={Styles.input}
           onPress={this.onLogin.bind(this)}
         />
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#ecf0f1',
-  },
-  input: {
-    width: 200,
-    height: 44,
-    padding: 10,
-    borderWidth: 1,
-    borderColor: 'black',
-    marginBottom: 10,
-  },
-});
